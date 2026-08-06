464813653a2c0aaa5feeb142ac198125acd43271
Adrian506-coder <treviadrian18@gmail.com>
2026-08-06 11:06:12 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API = "https://transport-situations-carmen-midi.trycloudflare.com/test/api/index.php";
+const API = "https://dfhash.com/temporal/practicasDDI/biblioteca/api/index.php";
 
 let modalErrorLogin = null;
 if (document.getElementById("exampleModal")) {
