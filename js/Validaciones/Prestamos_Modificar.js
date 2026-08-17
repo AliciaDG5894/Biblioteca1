d6a8189984170ae00403f493799c0eee5ee37ae6
AliciaDG5894 <alis200548@gmail.com>
2026-08-17 12:16:17 -0500

    Add validation messages for 'matricula' field

--- js/Validaciones/Prestamos_Modificar.js
+++ js/Validaciones/Prestamos_Modificar.js
@@ -45,7 +45,7 @@ $("#modificarPrestamo").validate({
     errorClass: "v_error", // Estilo para errores
     validClass: "v_correcto",   // Estilo para válidos
     messages: {
-        nombre: {
+        matricula: {
             required: "Por favor, llene este campo",
             minlength: "Por favor, ingrese de 8 caracteres",
             maxlength: "Por favor, no ingrese más de 8 caracteres",
